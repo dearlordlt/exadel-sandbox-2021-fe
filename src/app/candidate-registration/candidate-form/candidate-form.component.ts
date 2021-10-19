4c149c42174bc21944724f9bea8c92fe2602dc08
Saltenis Simonas (Student Com20) <simonas.saltenis@stud-inf.unibz.it>
2021-10-19 14:48:00 +0300

    fixed whitespace in candidate-registration page

--- src/app/candidate-registration/candidate-form/candidate-form.component.ts
+++ src/app/candidate-registration/candidate-form/candidate-form.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-candidate-form',
   templateUrl: './candidate-form.component.html',
-  styleUrls: ['./candidate-form.component.sass']
+  styleUrls: ['./candidate-form.component.sass'],
 })
 export class CandidateFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
