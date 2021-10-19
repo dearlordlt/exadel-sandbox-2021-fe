359952f854fe047cb6ffe7b2e4d70528c465c919
Saltenis Simonas (Student Com20) <simonas.saltenis@stud-inf.unibz.it>
2021-10-19 14:57:25 +0300

    fixed sass to scss conflicts

--- src/app/candidate-registration/candidate-form/candidate-form.component.ts
+++ src/app/candidate-registration/candidate-form/candidate-form.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-candidate-form',
   templateUrl: './candidate-form.component.html',
-  styleUrls: ['./candidate-form.component.sass'],
+  styleUrls: ['./candidate-form.component.scss'],
 })
 export class CandidateFormComponent implements OnInit {
   constructor() {}
--- src/app/candidate-registration/candidate-registration/candidate-registration.component.ts
+++ src/app/candidate-registration/candidate-registration/candidate-registration.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-candidate-registration',
   templateUrl: './candidate-registration.component.html',
-  styleUrls: ['./candidate-registration.component.sass']
+  styleUrls: ['./candidate-registration.component.scss'],
 })
 export class CandidateRegistrationComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
