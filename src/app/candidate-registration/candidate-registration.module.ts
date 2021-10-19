ca28a52b8998d882b40f3ecb78501938d9618a77
Saltenis Simonas (Student Com20) <simonas.saltenis@stud-inf.unibz.it>
2021-10-19 18:59:03 +0300

    changed candidate-registration componenet according to new requirements

--- src/app/candidate-registration/candidate-registration.module.ts
+++ src/app/candidate-registration/candidate-registration.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CandidateRegistrationComponent } from './candidate-registration/candidate-registration.component';
 import { CandidateFormComponent } from './candidate-form/candidate-form.component';
+import { EducationalProgramsComponent } from './educational-programs/educational-programs.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [CandidateRegistrationComponent, CandidateFormComponent],
-  imports: [CommonModule],
+  declarations: [CandidateRegistrationComponent, CandidateFormComponent, EducationalProgramsComponent],
+  imports: [CommonModule, MatButtonModule, MatCardModule],
 })
 export class CandidateRegistrationModule {}
