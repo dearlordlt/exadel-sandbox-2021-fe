8b4d97fd839a5596262c88e073800d9e2392dc4e
Sarvar <r0ssmike727@yandex.com>
2021-10-23 01:26:21 +0500

    add: mat-table

---
+++ src/app/app-material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatTableModule, MatPaginatorModule, MatInputModule],
+})
+export class AppMaterialModule {}
--- src/app/components/dashboard/candidate-list/candidate-list.component.ts
+++ src/app/components/dashboard/candidate-list/candidate-list.component.ts
@@ -1,23 +1,46 @@
-import {Component, OnInit} from '@angular/core';
-import {filter, map, switchMap, take} from "rxjs/operators";
-import {from} from "rxjs";
-import {CandidatesService} from "./services/candidates.service";
-import {ActivatedRoute} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { filter, map, switchMap, take } from 'rxjs/operators';
+import { from } from 'rxjs';
+import { CandidatesService } from './services/candidates.service';
+import { ActivatedRoute } from '@angular/router';
+import { Candidate } from '../../models/candidate';
 
 @Component({
   selector: 'app-candidate-list',
   templateUrl: './candidate-list.component.html',
-  styleUrls: ['./candidate-list.component.scss']
+  styleUrls: ['./candidate-list.component.scss'],
 })
 export class CandidateListComponent implements OnInit {
+  candidates: Candidate[] = [];
 
-  candidates$ = this.candidatesService.all();
+  displayedColumns: string[] = [
+    'firstname',
+    'lastname',
+    'eduProg',
+    'position',
+    'email',
+    'phone_number',
+    'country',
+    'english_level',
+    'contact_time',
+    'plan_to_join',
+    'date_of_apply',
+    'status',
+    'soft_skill',
+    'hard_skill',
+    'mentor_mark',
+    'interviewer_mark',
+  ];
 
-  constructor(private candidatesService: CandidatesService) {
-  }
+  // assessment: string[] = ['soft_skill', 'hard_skill', 'mentor_mark', 'interviewer_mark'];
+
+  constructor(private candidatesService: CandidatesService) {}
 
   ngOnInit(): void {
-
+    this.getCandidates();
   }
 
+  getCandidates(): void {
+    this.candidatesService.getCandidates().subscribe((candidates) => (this.candidates = candidates));
+  }
 }
