3572c99d39f00747ee8c3e93a7d7b8621c54f066
Mirzakobil <mirzakobilanvarov@gmail.com>
2021-10-26 02:42:39 +0500

    add ng material select edu programs

--- src/app/candidate-registration/candidate-form/candidate-form.component.ts
+++ src/app/candidate-registration/candidate-form/candidate-form.component.ts
@@ -1,11 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Program {
+  value: string;
+  viewValue: string;
+}
+interface Technology{
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-candidate-form',
   templateUrl: './candidate-form.component.html',
   styleUrls: ['./candidate-form.component.scss'],
 })
 export class CandidateFormComponent implements OnInit {
+  programs: Program[] = [
+    {value: '.Net-0', viewValue: '.Net'},
+    {value: 'JS-1', viewValue: 'JS'},
+    {value: 'BA-2', viewValue: 'BS'}
+  ];
+  technologies: Technology[] = [
+    {value: '.Net-0', viewValue: '.Net'},
+    {value: 'JS-1', viewValue: 'JS'},
+    {value: 'BA-2', viewValue: 'BS'}
+  ]
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/candidate-registration/candidate-registration.module.ts
+++ src/app/candidate-registration/candidate-registration.module.ts
@@ -4,9 +4,11 @@ import { CandidateFormComponent } from './candidate-form/candidate-form.componen
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CandidateHeaderComponent } from './candidate-header/candidate-header.component';
-
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select'
 @NgModule({
   declarations: [CandidateFormComponent, CandidateHeaderComponent],
-  imports: [CommonModule, MatButtonModule, MatCardModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatSelectModule],
 })
 export class CandidateRegistrationModule {}
