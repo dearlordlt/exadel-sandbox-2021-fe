4d4dd61fad4463591189e1585a624c500f52bd35
Mirzakobil <mirzakobilanvarov@gmail.com>
2021-10-27 02:08:57 +0500

    finish reg form

--- src/app/candidate-registration/candidate-registration.module.ts
+++ src/app/candidate-registration/candidate-registration.module.ts
@@ -5,10 +5,14 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { CandidateHeaderComponent } from './candidate-header/candidate-header.component';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatSelectModule } from '@angular/material/select'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox'
+// import { MatSelectCountryModule } from "@angular-material-extensions/select-country";
+// import {HttpClientModule} from '@angular/common/http';
 @NgModule({
   declarations: [CandidateFormComponent, CandidateHeaderComponent],
-  imports: [CommonModule, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatSelectModule],
+  imports: [CommonModule, MatButtonModule, MatCardModule, MatInputModule, MatFormFieldModule, MatSelectModule, MatCheckboxModule], 
+  // MatSelectCountryModule.forRoot('en'), HttpClientModule],
 })
 export class CandidateRegistrationModule {}
