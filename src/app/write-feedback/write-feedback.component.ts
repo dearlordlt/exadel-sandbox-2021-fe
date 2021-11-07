cb107f3293b3cae90a81cdea5968727851ca4ea6
Gzardiashvili <zardiashviligiorgi17@gmail.com>
2021-11-07 09:18:14 +0400

    write feedback

---
+++ src/app/write-feedback/write-feedback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-write-feedback',
+  templateUrl: './write-feedback.component.html',
+  styleUrls: ['./write-feedback.component.scss']
+})
+export class WriteFeedbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
