6b5d386acc2866e01f8e2c5bc724d26cf0824532
Mirzakobil <mirzakobilanvarov@gmail.com>
2021-11-10 00:56:43 +0500

    add btn back to reg page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, NgForm, Validators} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  email = new FormControl('', [Validators.required, Validators.email, Validators.pattern(/@exadel/)]);
+  // username = '';
+  // password1 = '';
+  // isValidName = true;
+  // isValidPassword = true;
+  // isBtnEnabled = false;
+  // onType(event: any) {
+  //   if (event.target.id === 'input--username') {
+  //     this.username = event.target.value;
+  //     this.isValidName = this.username.match(/@exadel.com$/) ? true : false;
+  //   }
+
+  //   if (event.target.id === 'input--password') {
+  //     this.password1 = event.target.value;
+  //     this.isValidPassword = this.password1.length > 5 ? true : false;
+  //   }
+  //   this.isBtnEnabled = this.isValidName && this.isValidPassword;
+  //   console.log(this.isBtnEnabled);
+  // }
+  email = new FormControl('', [Validators.required, Validators.email, Validators.pattern(/@exadel.com/)]);
   password = new FormControl('',  [Validators.required, Validators.minLength(6)])
   hide = true;
   getErrorMessage() {
@@ -14,7 +32,7 @@ export class LoginComponent implements OnInit {
       return 'You must enter a email';
     }
     if (this.email.hasError('pattern')) {
-      return 'You must enter proper email containing @exadel';
+      return 'You must enter proper email containing @exadel.com';
     }
     if (this.password.hasError('minLength')) {
       return 'Your password must be more than 6 symbols'
@@ -36,4 +54,9 @@ export class LoginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(logInForm: NgForm){
+    console.log(logInForm.value)
+    // console.log(this.username);
+    // console.log(this.password1);
+  }
 }
