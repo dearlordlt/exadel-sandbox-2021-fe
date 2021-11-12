1821bbcb30a589c8da4ed06d35a821543705abe6
mikheil-gel <mikheil.gelenidze13b@geolab.edu.ge>
2021-11-12 15:03:14 +0400

    move mat-modules into app-material.module

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -8,10 +8,36 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatSortModule } from '@angular/material/sort';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [MatTableModule, MatPaginatorModule, MatInputModule, MatIconModule, MatSelectModule, MatTooltipModule, MatProgressSpinnerModule],
+  exports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatIconModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatDialogModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatSortModule,
+    MatCheckboxModule,
+  ],
 })
 export class AppMaterialModule {}
