79a40b445f552a93b82b56d2c3cf0b25ade794ae
Mikheil Gelenidze <51454865+mikheil-gel@users.noreply.github.com>
2021-11-12 16:37:43 +0400

    move mat-modules into app-material.module (#31)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppMaterialModule } from './app-material.module';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CandidateRegistrationModule } from './candidate-registration/candidate-registration.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CandidateRegistrationModule } from './candidate-registration/candidate-registration.module';
+
+import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TopBarComponent } from './components/shared/top-bar/top-bar.component';
 import { SearchCandidateComponent } from './components/dashboard/search-candidate/search-candidate.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CandidateListComponent } from './components/dashboard/candidate-list/candidate-list.component';
-import { HttpClientModule } from '@angular/common/http';
 import { PlanningComponent } from './components/planning/planning.component';
 import { EducationalProgramsComponent } from './components/educational-programs/educational-programs.component';
 import { LettersComponent } from './components/letters/letters.component';
@@ -25,13 +20,8 @@ import { SendLetterComponent } from './components/letters/send-letter/send-lette
 import { ReportComponent } from './components/report/report.component';
 import { CreateEducationalProgramComponent } from './components/educational-programs/create-educational-program/create-educational-program.component';
 import { LogoutComponent } from './components/shared/logout/logout.component';
-import { MatDialogModule } from '@angular/material/dialog';
 import { LoginComponent } from './login/login.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { CreateLettersTempComponent } from './components/letters/create-letters-temp/create-letters-temp.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatSortModule } from '@angular/material/sort';
 import { SearchCandidatesComponent } from './components/shared/search-candidates/search-candidates.component';
 import { UpdateCandidateDialogComponent } from './components/dashboard/candidate-list/update-candidate-dialog/update-candidate-dialog.component';
 import { PlannerComponent } from './components/planning/planner/planner.component';
@@ -63,18 +53,7 @@ import { PlannerComponent } from './components/planning/planner/planner.componen
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatNativeDateModule,
     CandidateRegistrationModule,
-    RouterModule,
-    MatDialogModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatSortModule,
     FormsModule,
   ],
   providers: [],
