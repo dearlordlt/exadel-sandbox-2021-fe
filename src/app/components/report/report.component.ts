7b03755e347e44fe3ff063049deaa4053b95399e
Gzardiashvili <zardiashviligiorgi17@gmail.com>
2021-11-14 23:47:02 +0400

    feat/report - fix styles, add data visualization

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {educationalPrograms} from "../../global/constants";
 import {ReportService} from "./report.service";
-import {tap} from "rxjs/operators";
+import {filter, map, tap} from "rxjs/operators";
+import {Chart} from "angular-highcharts";
 
 @Component({
   selector: 'app-report',
@@ -11,11 +11,76 @@ import {tap} from "rxjs/operators";
 export class ReportComponent implements OnInit {
   programs$ = this.reportService.getPrograms();
   report$ = this.reportService.getReport(1);
+  reportArrOptions: string[] = ["Applications",
+    "Refusal by the level of English",
+    "Refusal by location",
+    "Failure on the technical level",
+    "Candidate's refusal",
+    "Sandbox completed"];
+  reportArrValues: number[] = [];
+  name: string = 'NaN';
+  chart!: Chart;
 
   constructor(private reportService: ReportService) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.init()
   }
 
+  createReport(id: number) {
+    this.reportService.getReport(id).pipe(tap(r => {
+      this.reportArrValues = Object.values(r[0])
+    })).subscribe();
+    this.reportService.getPrograms().pipe(tap(p => {
+      this.name = p[id - 1].name
+    })).subscribe()
+    this.report$ = this.reportService.getReport(id)
+  }
+
+  init() {
+    let chart = new Chart({
+      chart: {
+        type: 'bar'
+      },
+      credits: {
+        enabled: false
+      },
+      title: {
+        text: ''
+      },
+      yAxis: {
+        visible: true
+      },
+      legend: {
+        enabled: true,
+      },
+      xAxis: {
+        lineColor: '#fff',
+        categories: this.reportArrOptions
+      },
+      plotOptions: {
+        series: {
+          borderRadius: 5,
+        } as any
+      },
+      series: [
+        {
+          type: 'bar',
+          color: '#72b5e9',
+          name: this.name,
+          data: [
+            {y: this.reportArrValues[0]},
+            {y: this.reportArrValues[1]},
+            {y: this.reportArrValues[2]},
+            {y: this.reportArrValues[3]},
+            {y: this.reportArrValues[4]},
+            {y: this.reportArrValues[5]},
+          ]
+        }
+      ]
+    });
+    this.chart = chart;
+    chart.ref$.subscribe();
+  }
 }
