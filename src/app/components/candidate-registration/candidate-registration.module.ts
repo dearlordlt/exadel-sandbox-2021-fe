791d7909da8049237a7820751d2e04e5661b3f98
Mikheil Gelenidze <51454865+mikheil-gel@users.noreply.github.com>
2021-11-16 20:40:07 +0400

    move candidate-registration to components (#34)

--- src/app/components/candidate-registration/candidate-registration.module.ts
+++ src/app/components/candidate-registration/candidate-registration.module.ts
@@ -3,14 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MatSelectCountryModule } from '@angular-material-extensions/select-country';
 import { HttpClientModule } from '@angular/common/http';
-import { AppMaterialModule } from '../app-material.module';
-import { AppRoutingModule } from '../app-routing.module';
+import { AppMaterialModule } from '../../app-material.module';
+import { AppRoutingModule } from '../../app-routing.module';
 
 import { CandidateFormComponent } from './candidate-form/candidate-form.component';
 import { CandidateHeaderComponent } from './candidate-header/candidate-header.component';
+import { CandidateRegistrationComponent } from './candidate-registration.component';
 
 @NgModule({
-  declarations: [CandidateFormComponent, CandidateHeaderComponent],
+  declarations: [CandidateFormComponent, CandidateHeaderComponent, CandidateRegistrationComponent],
   imports: [CommonModule, AppRoutingModule, AppMaterialModule, MatSelectCountryModule.forRoot('en'), HttpClientModule],
 })
 export class CandidateRegistrationModule {}
