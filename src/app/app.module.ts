61d0a8cf1946ee54c4e138be3f8522cc3937d46b
mikheil-gel <mikheil.gelenidze13b@geolab.edu.ge>
2021-11-16 16:03:21 +0400

    move candidate-registration to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppMaterialModule} from './app-material.module';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {CandidateRegistrationModule} from './candidate-registration/candidate-registration.module';
+import { AppMaterialModule } from './app-material.module';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CandidateRegistrationModule } from './components/candidate-registration/candidate-registration.module';
 
-import {AppComponent} from './app.component';
-import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {TopBarComponent} from './components/shared/top-bar/top-bar.component';
-import {SearchCandidateComponent} from './components/dashboard/search-candidate/search-candidate.component';
-import {CandidateListComponent} from './components/dashboard/candidate-list/candidate-list.component';
-import {PlanningComponent} from './components/planning/planning.component';
-import {EducationalProgramsComponent} from './components/educational-programs/educational-programs.component';
-import {LettersComponent} from './components/letters/letters.component';
-import {SendLetterComponent} from './components/letters/send-letter/send-letter.component';
-import {ReportComponent} from './components/report/report.component';
-import {CreateEducationalProgramComponent} from './components/educational-programs/create-educational-program/create-educational-program.component';
-import {LogoutComponent} from './components/shared/logout/logout.component';
-import {LoginComponent} from './login/login.component';
-import {CreateLettersTempComponent} from './components/letters/create-letters-temp/create-letters-temp.component';
-import {SearchCandidatesComponent} from './components/shared/search-candidates/search-candidates.component';
-import {UpdateCandidateDialogComponent} from './components/dashboard/candidate-list/update-candidate-dialog/update-candidate-dialog.component';
-import {PlannerComponent} from './components/planning/planner/planner.component';
-import {ChartModule} from "angular-highcharts";
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TopBarComponent } from './components/shared/top-bar/top-bar.component';
+import { SearchCandidateComponent } from './components/dashboard/search-candidate/search-candidate.component';
+import { CandidateListComponent } from './components/dashboard/candidate-list/candidate-list.component';
+import { PlanningComponent } from './components/planning/planning.component';
+import { EducationalProgramsComponent } from './components/educational-programs/educational-programs.component';
+import { LettersComponent } from './components/letters/letters.component';
+import { SendLetterComponent } from './components/letters/send-letter/send-letter.component';
+import { ReportComponent } from './components/report/report.component';
+import { CreateEducationalProgramComponent } from './components/educational-programs/create-educational-program/create-educational-program.component';
+import { LogoutComponent } from './components/shared/logout/logout.component';
+import { LoginComponent } from './login/login.component';
+import { CreateLettersTempComponent } from './components/letters/create-letters-temp/create-letters-temp.component';
+import { SearchCandidatesComponent } from './components/shared/search-candidates/search-candidates.component';
+import { UpdateCandidateDialogComponent } from './components/dashboard/candidate-list/update-candidate-dialog/update-candidate-dialog.component';
+import { PlannerComponent } from './components/planning/planner/planner.component';
+import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
   declarations: [
@@ -56,11 +56,10 @@ import {ChartModule} from "angular-highcharts";
     BrowserAnimationsModule,
     CandidateRegistrationModule,
     FormsModule,
-    ChartModule
+    ChartModule,
   ],
   providers: [],
   entryComponents: [LogoutComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
