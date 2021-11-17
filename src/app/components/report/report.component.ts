74c38ccc03931d778f016e8d91877a6bad6a7b92
Gzardiashvili <zardiashviligiorgi17@gmail.com>
2021-11-17 18:05:36 +0400

    feat/save-to-excel - create feature to export in excel file

--- src/app/components/report/report.component.ts
+++ src/app/components/report/report.component.ts
@@ -1,22 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {ReportService} from "./report.service";
-import {filter, map, tap} from "rxjs/operators";
-import {Chart} from "angular-highcharts";
+import { Component, OnInit } from '@angular/core';
+import { ReportService } from './report.service';
+import { filter, map, tap } from 'rxjs/operators';
+import { Chart } from 'angular-highcharts';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+  styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit {
   programs$ = this.reportService.getPrograms();
   report$ = this.reportService.getReport(1);
-  reportArrOptions: string[] = ["Applications",
-    "Refusal by the level of English",
-    "Refusal by location",
-    "Failure on the technical level",
-    "Candidate's refusal",
-    "Sandbox completed"];
+  fileName = 'Report.xlsx';
+  reportArrOptions: string[] = ['Applications',
+    'Refusal by the level of English',
+    'Refusal by location',
+    'Failure on the technical level',
+    'Candidate\'s refusal',
+    'Sandbox completed'];
   reportArrValues: number[] = [];
   name = 'NaN';
   chart!: Chart;
@@ -25,44 +27,44 @@ export class ReportComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.init()
+    this.init();
   }
 
   createReport(id: number) {
     this.reportService.getReport(id).pipe(tap(r => {
-      this.reportArrValues = Object.values(r[0])
+      this.reportArrValues = Object.values(r[0]);
     })).subscribe();
     this.reportService.getPrograms().pipe(tap(p => {
-      this.name = p[id - 1].name
-    })).subscribe()
-    this.report$ = this.reportService.getReport(id)
+      this.name = p[id - 1].name;
+    })).subscribe();
+    this.report$ = this.reportService.getReport(id);
   }
 
   init() {
     const chart = new Chart({
       chart: {
-        type: 'bar'
+        type: 'bar',
       },
       credits: {
-        enabled: false
+        enabled: false,
       },
       title: {
-        text: ''
+        text: '',
       },
       yAxis: {
-        visible: true
+        visible: true,
       },
       legend: {
         enabled: true,
       },
       xAxis: {
         lineColor: '#fff',
-        categories: this.reportArrOptions
+        categories: this.reportArrOptions,
       },
       plotOptions: {
         series: {
           borderRadius: 5,
-        } as any
+        } as any,
       },
       series: [
         {
@@ -70,17 +72,28 @@ export class ReportComponent implements OnInit {
           color: '#72b5e9',
           name: this.name,
           data: [
-            {y: this.reportArrValues[0]},
-            {y: this.reportArrValues[1]},
-            {y: this.reportArrValues[2]},
-            {y: this.reportArrValues[3]},
-            {y: this.reportArrValues[4]},
-            {y: this.reportArrValues[5]},
-          ]
-        }
-      ]
+            { y: this.reportArrValues[0] },
+            { y: this.reportArrValues[1] },
+            { y: this.reportArrValues[2] },
+            { y: this.reportArrValues[3] },
+            { y: this.reportArrValues[4] },
+            { y: this.reportArrValues[5] },
+          ],
+        },
+      ],
     });
     this.chart = chart;
     chart.ref$.subscribe();
   }
+
+  exportExcel(): void {
+    if (this.name != 'NaN') {
+      const element = document.getElementById('excel-table');
+      const ws = XLSX.utils.table_to_sheet(element);
+      const wb: XLSX.WorkBook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+      XLSX.writeFile(wb, this.fileName);
+    }
+  }
+
 }
