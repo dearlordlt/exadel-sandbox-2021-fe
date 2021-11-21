36a743a7d098ff2873b7a6b04111b8ca8e98aff3
mikheil-gel <mikheil.gelenidze13b@geolab.edu.ge>
2021-11-21 14:10:18 +0400

    fix auth.guard

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,24 +1,16 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
-import { AuthenticationService } from '../service/authentication/authentication.service';
-import { Router } from '@angular/router';
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
-  constructor(private authenticationService: AuthenticationService, private router: Router){
+import { TestBed } from '@angular/core/testing';
 
-  }
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.authenticationService.isAuthenticated){
-        return true;
-      }
-      this.router.navigate(['login']);
-      return false;
-  }
-  
-}
+import { AuthGuard } from './auth.guard';
 
+describe('AuthGuard', () => {
+  let guard: AuthGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    guard = TestBed.inject(AuthGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+});
