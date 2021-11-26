42686a88c800a611cb91e570fb67207cd4946b91
Gzardiashvili <zardiashviligiorgi17@gmail.com>
2021-11-26 18:15:36 +0400

    fix api url

--- src/app/service/http/report/report.service.ts
+++ src/app/service/http/report/report.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import { EducationalProgram } from '../../../components/shared/interfaces/educational-program/educational-program.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -10,10 +11,10 @@ export class ReportService {
   }
 
   getPrograms() {
-    return this.http.get<any>(`${environment.EXADEL_API}/educationalPrograms`);
+    return this.http.get<EducationalProgram[]>(`${environment.EXADEL_API}/EducationProgram/GetAllEducationProgramWithSortAndFilter`);
   }
 
   getReport(id: number) {
-    return this.http.get<any>(`${environment.EXADEL_API}/GetForReport?eduProgId=${id}`);
+    return this.http.get<any>(`${environment.EXADEL_API}/Candidates/GetForReport?eduProgId=${id}`);
   }
 }
