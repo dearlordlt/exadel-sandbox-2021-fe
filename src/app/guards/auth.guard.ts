62ccbdb8ae1443f4c7fa239a71a636b306ff4fc8
Gzardiashvili <zardiashviligiorgi17@gmail.com>
2021-11-28 08:10:45 +0400

    api

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -15,6 +15,7 @@ export class AuthGuard implements CanActivate {
     if (this.authenticationService.isAuthenticated) {
       return true;
     }
+
     this.router.navigate(['login']);
     return false;
   }
