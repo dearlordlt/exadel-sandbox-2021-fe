1c8366d74f10d0c946e8c817cc077bd9f1420d97
Mirzakobil <mirzakobilanvarov@gmail.com>
2021-11-29 23:13:50 +0500

    add form input valudator

--- src/app/components/candidate-registration/candidate-form/candidate-form.component.ts
+++ src/app/components/candidate-registration/candidate-form/candidate-form.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { educationalPrograms, englishLevels } from 'src/app/global/constants';
 import { countries } from 'src/app/global/constants';
 import { contactTime } from 'src/app/global/constants';
 import { decision } from 'src/app/global/constants'
-import { technologies } from 'src/app/global/constants';
-import { technologyJsJava } from 'src/app/global/constants';
-import { technologyJsNET } from 'src/app/global/constants';
-import { technologyJsPHP } from 'src/app/global/constants';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+// import { technologies } from 'src/app/global/constants';
+// import { technologyJsJava } from 'src/app/global/constants';
+// import { technologyJsNET } from 'src/app/global/constants';
+// import { technologyJsPHP } from 'src/app/global/constants';
 @Component({
   selector: 'app-candidate-form',
   templateUrl: './candidate-form.component.html',
@@ -19,23 +21,26 @@ export class CandidateFormComponent implements OnInit {
     lastName: ['', [Validators.required]],
     skype: ['', [Validators.required]],
     email: ['', [Validators.required, Validators.email]],
-    phoneNumber: ['', [Validators.required, Validators.pattern(/1-9/)]],
+    phoneNumber: ['', [Validators.required, Validators.pattern(/[0-9]{12}/)]],
     country: ['', [Validators.required, Validators.pattern(/[a-zA-Z ]*/)]],
     city: ['', [Validators.required, Validators.pattern(/[a-zA-Z ]*/)]],
     engLevel: ['', [Validators.required]],
     contactTime: ['', [Validators.required]],
     decision: ['', [Validators.required]],
-    checkBoxd: ['', [Validators.required]],
+    checkBox: ['', [Validators.required]],
   });
   programs = educationalPrograms;
-  technologies = technologies;
+  //technologies = technologies;
   levels = englishLevels;
   countries = countries;
   contactTimes = contactTime;
   decisions = decision;
-  
 
-  constructor(private fb: FormBuilder) {}
+  
+  constructor(private fb: FormBuilder,private http: HttpClient) {}
 
   ngOnInit(): void {}
+  onSubmit(){
+    console.log(this.registrationForm.value)
+  }
 }
