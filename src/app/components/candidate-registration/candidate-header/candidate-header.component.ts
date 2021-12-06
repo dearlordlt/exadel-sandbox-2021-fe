ffaa112a3277e91a3a513e5fa18ccb7feb71e1f1
simassimonas <simonassaltenis@gmail.com>
2021-12-06 12:57:18 +0200

    add scroll into view functionality

--- src/app/components/candidate-registration/candidate-header/candidate-header.component.ts
+++ src/app/components/candidate-registration/candidate-header/candidate-header.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-candidate-header',
   templateUrl: './candidate-header.component.html',
-  styleUrls: ['./candidate-header.component.scss']
+  styleUrls: ['./candidate-header.component.scss'],
 })
 export class CandidateHeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  scrollToCandidateRegistrationPart(id: string) {
+    const element = document.getElementById(id);
+    if (element) element.scrollIntoView({ behavior: 'smooth' });
   }
-
 }
