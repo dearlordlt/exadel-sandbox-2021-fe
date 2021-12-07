a08539734997f14611219fd9d9b1648344327f27
mikheil-gel <mikheilgelenidze@gmail.com>
2021-12-07 09:58:07 +0400

    fix code after api change

--- src/app/components/models/employeeData.ts
+++ src/app/components/models/employeeData.ts
@@ -1,14 +1,9 @@
-//NOT FINAL FORM, OTHER INTERFACES NEED TO BE READY FIRST
 export interface EmployeeData {
   id: string;
   firstname: string;
   lastname: string;
   email: string;
   passw: string;
+  empPosition: string;
   roleId: string;
-  role: {
-    id: string;
-    roleName: string;
-    employees: [];
-  };
 }
